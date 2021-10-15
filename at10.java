e1399f161e897ee9d77435030f9a4a1c5bac6e8e
sanmartin21 <jvsanmartin@yahoo.com.br>
2021-10-15 19:46:15 -0300

    at03

--- at10.java
+++ at10.java
@@ -1,24 +1,23 @@
 
-
 import java.util.Scanner;
 
 public class at10 {
 
 	public static void main(String[] args) {
 
-
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Insira o valor da base.");
 		int base = input.nextInt();
 		System.out.println("Insira o valor do expoente.");
 		int expoente = input.nextInt();
-		
-		 for(int i=0 ; i < 1; i++){
-			 double resultado = Math.pow (base,expoente);
-			 System.out.println(resultado);
-		    }
-		 input.close();
-	}
 
+		int resultado = 1;
+		for (int i = 0; i < expoente; i++) {
+			resultado *= base;
+		}
+		System.out.println("Resultado: " + resultado);
+
+		input.close();
+	}
 }
