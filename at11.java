015d3d27e8ada44b43731a004af957ed81afd998
sanmartin21 <jvsanmartin@yahoo.com.br>
2021-10-15 20:52:05 -0300

    at03

--- at11.java
+++ at11.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 public class at11 {
@@ -8,20 +7,21 @@ public class at11 {
 
 		Scanner input = new Scanner(System.in);
 
-		int num = 0; int impar = 0; int par = 0;
-		
+		int num = 0;
+		int impar = 0;
+		int par = 0;
 
 		for (int x = 0; x < 10; x++) {
 			System.out.println("Insira um número inteiro:");
 			num = input.nextInt();
 			if (num % 2 != 0) {
-				impar = impar +1;
+				impar = impar + 1;
 			} else
-				par = par +1;
+				par = par + 1;
 		}
 		System.out.println("Impares:" + impar);
 		System.out.println("Pares:" + par);
-		
+
 		input.close();
 	}
 }
---
+++ at33.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class at33 {
+
+	public static void main(String[] args) {
+
+
+		Scanner input = new Scanner(System.in);
+		
+		double div;
+		int juros;
+		int parcela1 = 1;
+		int parcela2 = 3;
+		int parcela3 = 6;
+		int parcela4 = 9;
+		int parcela5 = 12;
+		double valor1, valor2, valor3, valor4, valor5;
+		double juros1, juros2, juros3, juros4, juros5;
+		
+		System.out.println("Digite o valor da dívida: ");
+		div = input.nextDouble();
+		
+		juros1 = 0;
+		juros2 = ((div/100)*10);
+		juros3 = ((div/100)*15);
+		juros4 = ((div/100)*20);	
+		juros5 = ((div/100)*25);
+		
+		valor1 = (div+juros1)/1;
+		valor2 = (div+juros2)/3;
+		valor3 = (div+juros3)/6;
+		valor4 = (div+juros4)/9;
+		valor5 = (div+juros5)/12;
+		
+		System.out.println("Valor da dívida    Valor dos Juros   Quantidade de parcelas     Valor da parcela");
+		
+		for(int i = 1; i <= 5; i++) {
+			if( i == 1) 
+				System.out.println("R$ " + div + " ------------  " + juros1 + " ------------------- " + parcela1 + " --------------- R$ " + valor1);
+			else if(i == 2)
+				System.out.println("R$ " + div + " ------------  " + juros2 + " ------------------- " + parcela2 + " --------------- R$ " + valor2);
+			else if(i == 3)
+				System.out.println("R$ " + div + " ------------  " + juros3 + " ------------------- " + parcela3 + " --------------- R$ " + valor3);
+			else if(i == 4)
+				System.out.println("R$ " + div + " ------------  " + juros4 + " ------------------- " + parcela4 + " --------------- R$ " + valor4);
+			else if(i == 5)
+				System.out.println("R$ " + div + " ------------  " + juros5 + " ------------------- " + parcela5 + " --------------- R$ " + valor5);
+		}
+			
+		input.close();
+			 
+	}
+
+}
+
+
+	
